52437019655e95dd94f5f86a3109f959d7641e63
Durga-Kondaveeti <87610106+Durga-Kondaveeti@users.noreply.github.com>
2025-01-22 18:33:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (60.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,10 +2,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         prev = 0
         cur = 1
-        ans = 0
         for i in range(n):
-            ans = prev + cur
-            prev = cur
-            cur = ans 
-        return ans
-            
+            sample = cur
+            cur = prev + cur 
+            prev = sample
+        return cur
