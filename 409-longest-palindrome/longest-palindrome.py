75f92732492e2970aeb50672399bfa09f3e0db8c
Durga-Kondaveeti <87610106+Durga-Kondaveeti@users.noreply.github.com>
2025-02-22 00:56:29 -0500

    Time: 7 ms (3.78%) | Memory: 18 MB (27.86%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        sample = []
-        length = 0
+        a = []
+        count=0
         for i in s:
-            if i in sample:
-                length+=2
-                sample.remove(i)
-
+            if i in a:
+                a.remove(i)
+                count+=2
             else:
-                sample.append(i)
-        if sample:
-            length+=1
-        return length
+                a.append(i)
+        if a:
+            return count+1
+        else:
+            return count
