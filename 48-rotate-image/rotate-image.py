71ca01891ef9aac5e4813d8526e8b56f5b05c641
Durga-Kondaveeti <87610106+Durga-Kondaveeti@users.noreply.github.com>
2025-02-25 00:45:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.37%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,9 +3,11 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for i in range(len(matrix)):
-            for j in range(i+1,len(matrix)):
+
+        length = len(matrix)
+        for i in range(length):
+            for j in range(i+1,length):
                 matrix[i][j], matrix[j][i] =  matrix[j][i], matrix[i][j]
-        for i in range(len(matrix)):
+        for i in range(length):
             matrix [i] = matrix[i][::-1]
         return matrix
