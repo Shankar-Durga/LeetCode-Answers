5a9e7719f47c20dbc392e89c00391c728304ec90
Durga-Kondaveeti <87610106+Durga-Kondaveeti@users.noreply.github.com>
2025-02-25 15:16:39 -0500

    Time: 238 ms (79.55%) | Memory: 20 MB (95.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,8 +3,7 @@ class Solution:
         grid[row][col] = 0
         lst = [(row-1,col),(row+1,col),(row,col-1),(row,col+1)]
         for i, j in lst:
-            if i>=0 and j>=0 and i<len(grid) and j<len(grid[0]):
-                if grid[i][j] == "1":
+            if i>=0 and j>=0 and i<len(grid) and j<len(grid[0]) and grid[i][j] == "1":                
                     self.island_checker(grid, i, j)
 
     def numIslands(self, grid: List[List[str]]) -> int:
