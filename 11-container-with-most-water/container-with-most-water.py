840607a0f5525283769249563c367cbb4cded1ea
Shankar-Durga <87610106+Shankar-Durga@users.noreply.github.com>
2025-04-05 11:32:33 -0400

    Time: 99 ms (69.25%) | Memory: 28.6 MB (24.09%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,15 +3,11 @@ class Solution:
         most_water = 0
         left = 0
         right = len(height)-1
-        checker = 0
         while left<right:
-
             min_value = min(height[left], height[right])
-            checker = min_value*(right - left)
-            most_water = max(checker, most_water)
+            most_water = max(min_value*(right - left), most_water)
             if height[left]<height[right]:
                 left += 1
             else:
-                right -=1
-            
+                right -=1  
         return most_water
