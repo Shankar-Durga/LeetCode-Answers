bb549915d0696bb18b307e9d4abf0ea3306bc07e
Shankar-Durga <87610106+Shankar-Durga@users.noreply.github.com>
2025-04-05 23:35:47 -0400

    Time: 47 ms (35.31%) | Memory: 18.2 MB (16.83%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,18 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def checker(self, tree, subRoot):
-        if tree is None and subRoot is None:
-            return True
-        if tree is None or subRoot is None:
-            return False
+    # def checker(self, tree, subRoot):
+    #     if tree is None and subRoot is None:
+    #         return True
+    #     if tree is None or subRoot is None:
+    #         return False
 
-        return tree.val == subRoot.val and self.checker(tree.left, subRoot.left) and self.checker(tree.right, subRoot.right)
+    #     return tree.val == subRoot.val and self.checker(tree.left, subRoot.left) and self.checker(tree.right, subRoot.right)
 
 
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if root is None:
             return False
-        return self.checker(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right, subRoot)
+        def checker( tree, subRoot_test):
+            if tree is None and subRoot_test is None:
+                return True
+            if tree is None or subRoot_test is None:
+                return False
+
+            return tree.val == subRoot_test.val and checker(tree.left, subRoot_test.left) and checker(tree.right, subRoot_test.right)
+        return checker(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right, subRoot)
 
             
