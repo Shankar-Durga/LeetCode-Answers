319f22fea3ca2c724f6b5c5c7db9f70b06cae5e9
Shankar-Durga <87610106+Shankar-Durga@users.noreply.github.com>
2025-04-06 19:49:33 -0400

    Time: 252 ms (52.71%) | Memory: 20.2 MB (58.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,16 +1,17 @@
 class Solution:
-    def island_checker(self, grid, row, col):
-        grid[row][col] = 0
-        lst = [(row-1,col),(row+1,col),(row,col-1),(row,col+1)]
-        for i, j in lst:
-            if i>=0 and j>=0 and i<len(grid) and j<len(grid[0]) and grid[i][j] == "1":                
-                    self.island_checker(grid, i, j)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
+        def checker(row,col):
+            grid[row][col] = 0
+            directions = [(row-1,col),(row+1,col),(row,col-1),(row,col+1)]
+            for r,c in directions:
+                if r>=0 and c>=0 and r<len(grid) and c<len(grid[0]) and grid[r][c] == "1":
+                    checker(r,c)
+
+        count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    self.island_checker(grid,i,j)
-                    islands += 1
-        return islands
+                    print(count)
+                    count +=1
+                    checker(i,j)
+        return count
